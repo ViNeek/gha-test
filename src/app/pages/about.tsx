32d5c4bc4c375faa7f027b2d72f1943e9b94ea4d
Vineek <nickvitsas@gmail.com>
2025-01-09 19:05:39 +0200

    add server side rendering page

--- src/app/pages/about.tsx
+++ src/app/pages/about.tsx
@@ -1,12 +1,7 @@
-// pages/about/index.js
-export async function getStaticProps() {
-  return {
-    props: {
-      title: 'About Page',
-    },
-  };
-}
-
-export default function About({ title }) {
-  return <h1>sdfsdfsdf{title}</h1>;
+export default function About() {
+  return (
+    <p>
+      Ludacris
+    </p>
+  );
 }
